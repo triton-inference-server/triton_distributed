d89126470a494e94ab312bfa573308dbff0ca75f
Ryan McCormick <rmccormick@nvidia.com>
2025-01-14 15:53:01 -0800

    Add dummy test to verify vllm patch installation

---
+++ container/deps/vllm/tests/test_version.py
@@ -0,0 +1,12 @@
+import pytest
+
+try:
+    import vllm
+except ImportError:
+    vllm = None
+
+
+@pytest.mark.skipif(vllm is None, reason="Skipping vllm tests, vllm not installed")
+def test_version():
+    # Verify that the image has the patched version of vllm
+    assert vllm.__version__ == "0.6.3.post2.dev16+gf61960ce"
