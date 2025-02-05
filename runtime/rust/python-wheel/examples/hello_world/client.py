3d5718b597b0e6c58300ee18b8aeaf113201d36d
Ryan McCormick <rmccormick@nvidia.com>
2025-02-05 09:32:25 -0800

    Add __init__.py to examples with duplicate file names like 'client.py' to WAR mypy complaints

--- runtime/rust/python-wheel/examples/hello_world/client.py
+++ runtime/rust/python-wheel/examples/hello_world/client.py
@@ -1,8 +1,7 @@
 import asyncio
 
 import uvloop
-
-from triton_distributed import DistributedRuntime, triton_worker
+from triton_distributed_rs import DistributedRuntime, triton_worker
 
 
 @triton_worker()
