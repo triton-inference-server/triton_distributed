0e407cf509f94329ea1b8f8f7bf5f34756ba0a04
Ryan Olson <rolson@nvidia.com>
2025-02-10 13:37:40 -0800

    migrate examples

---
+++ examples/rust/http/src/main.rs
@@ -0,0 +1,9 @@
+use triton_distributed::{
+    protocols::annotated::Annotated, stream::StreamExt, DistributedRuntime, Result, Runtime, Worker,
+};
+
+fn main() -> Result<()> {
+    println!("Hello, world!");
+
+    Ok(())
+}
