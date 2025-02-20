404cd4b6acc7537b2affb0523f5b3befd7e428e9
Piotr Marcinkiewicz <piotrm@nvidia.com>
2025-02-20 18:17:19 +0100

    Update icp/python/src/triton_distributed/icp/memory_type.py
    Signed-off-by: Piotr Marcinkiewicz <piotrm@nvidia.com>

--- icp/python/src/triton_distributed/icp/memory_type.py
+++ icp/python/src/triton_distributed/icp/memory_type.py
@@ -20,7 +20,7 @@ MemoryType = IntEnum("MemoryType", names=("CPU", "CPU_PINNED", "GPU"), start=0)
 # Is more touching necessary to trigger pipeline?
 
 # It is necessary to trigger changes in Python to force valid gitlab execution.
-# Let's touch this file again to trigger tests.
+# Let's touch this file again and again to trigger tests.
 
 
 def string_to_memory_type(memory_type_string: str) -> MemoryType:
