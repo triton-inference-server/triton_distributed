4eaf82bb2ae045087fce89232e98e15def6c129e
Biswa Panda <biswa.panda@gmail.com>
2025-02-24 13:03:14 -0800

    feat: add python binding for tokenizer, preprocessor and model card

--- lib/bindings/python/src/triton_distributed/runtime/__init__.py
+++ lib/bindings/python/src/triton_distributed/runtime/__init__.py
@@ -19,9 +19,13 @@ from functools import wraps
 from typing import Any, AsyncGenerator, Callable, Type
 
 from pydantic import BaseModel, ValidationError
-
 from triton_distributed._core import Client as Client
 from triton_distributed._core import DistributedRuntime as DistributedRuntime
+from triton_distributed_rs._core import Backend as Backend
+from triton_distributed_rs._core import DistributedRuntime
+from triton_distributed_rs._core import KvRouter as KvRouter
+from triton_distributed_rs._core import ModelDeploymentCard as ModelDeploymentCard
+from triton_distributed_rs._core import OAIChatPreprocessor as OAIChatPreprocessor
 
 
 def triton_worker():
