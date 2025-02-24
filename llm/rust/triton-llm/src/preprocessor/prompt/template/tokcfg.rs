ebefeb12b289fa35bf82066c10a70ff6ccbbbeec
Biswa Panda <biswa.panda@gmail.com>
2025-02-23 19:54:46 -0800

    chore: clean up docs and unused obsolete code

--- llm/rust/triton-llm/src/preprocessor/prompt/template/tokcfg.rs
+++ llm/rust/triton-llm/src/preprocessor/prompt/template/tokcfg.rs
@@ -21,11 +21,6 @@ use either::Either;
 use minijinja::{value::Kwargs, Error, ErrorKind, Value};
 use serde::{Deserialize, Serialize};
 
-// const SUPPORTED_ALTERNATE_EOS: &[&str] = &[
-//     "<|im_end|>",    // Handle ChatML case
-//     "<end_of_turn>", // Handle Gemma2 chat case
-// ];
-
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct AddedTokensDecoder {
@@ -120,80 +115,6 @@ impl ChatTemplate {
     }
 }
 
-// pub fn calculate_eos_tokens(
-//     chat_template: &ChatTemplate,
-//     gen_conf: Option<GenerationConfig>,
-//     tokenizer: &Tokenizer,
-// ) -> Vec<u32> {
-//     let mut eos_tok_ids = chat_template.eos_tok().map(|x| vec![x]).unwrap_or_default();
-//     let mut bos_tok_ids = chat_template.bos_tok().map(|b| vec![b]).unwrap_or_default();
-
-//     for alternate in SUPPORTED_ALTERNATE_EOS {
-//         if tokenizer.get_vocab(true).contains_key(*alternate) {
-//             eos_tok_ids.push(alternate.to_string())
-//         }
-//     }
-
-//     if let Some(gen_conf) = gen_conf {
-//         let ids = match gen_conf.eos_token_id {
-//             Either::Left(id) => vec![id],
-//             Either::Right(ids) => ids,
-//         };
-//         for id in ids {
-//             let s = tokenizer
-//                 .decode(&[id], false)
-//                 .unwrap_or_else(|_| panic!("Unable to decode id {id})"));
-//             if !eos_tok_ids.contains(&s) {
-//                 eos_tok_ids.push(s);
-//             }
-//         }
-
-//         let ids = match gen_conf.bos_token_id {
-//             Either::Left(id) => vec![id],
-//             Either::Right(ids) => ids,
-//         };
-//         for id in ids {
-//             let s = tokenizer
-//                 .decode(&[id], false)
-//                 .unwrap_or_else(|_| panic!("Unable to decode id {id})"));
-//             if !bos_tok_ids.contains(&s) {
-//                 bos_tok_ids.push(s);
-//             }
-//         }
-//     }
-
-//     eos_tok_ids = eos_tok_ids.into_iter().dedup().collect::<Vec<_>>();
-//     bos_tok_ids = bos_tok_ids.into_iter().dedup().collect::<Vec<_>>();
-
-//     let bos_render = bos_tok_ids
-//         .iter()
-//         .map(|val| format!("{:?}", val))
-//         .collect::<Vec<String>>()
-//         .join(", ");
-//     let eos_render = eos_tok_ids
-//         .iter()
-//         .map(|val| format!("{:?}", val))
-//         .collect::<Vec<String>>()
-//         .join(", ");
-
-//     info!(
-//         "bos_toks = {bos_render}, eos_toks = {eos_render}, unk_tok = {}",
-//         chat_template.unk_tok().unwrap_or("`None`".to_string()),
-//     );
-
-//     let mut eos_toks = Vec::new();
-//     for eos_tok in eos_tok_ids {
-//         eos_toks.push(
-//             tokenizer
-//                 .get_vocab(true)
-//                 .get(&eos_tok)
-//                 .copied()
-//                 .unwrap_or_else(|| panic!("Unable to extract `{eos_tok}` EOS token.")),
-//         )
-//     }
-//     eos_toks
-// }
-
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct GenerationConfig {
@@ -236,82 +157,3 @@ pub fn tojson(value: Value, kwargs: Kwargs) -> Result<Value, Error> {
         Value::from_safe_string(rv)
     })
 }
-
-// pub fn apply_chat_template_to(
-//     messages: Vec<IndexMap<String, MessageContent>>,
-//     add_generation_prompt: bool,
-//     template: &ChatTemplateValue,
-//     bos_tok: Option<String>,
-//     eos_tok: Option<String>,
-//     unk_tok: Option<String>,
-//     tools: Vec<Tool>,
-// ) -> Result<String> {
-//     let mut env = Environment::new();
-
-//     // enable python methods such as .strip()
-//     env.set_unknown_method_callback(minijinja_contrib::pycompat::unknown_method_callback);
-
-//     // https://github.com/huggingface/transformers/blob/76a33a10923ccc1074917f6b6a1e719e626b7dc9/src/transformers/tokenization_utils_base.py#L1842
-//     env.set_lstrip_blocks(true);
-//     env.set_trim_blocks(true);
-
-//     #[derive(Serialize, Deserialize)]
-//     struct UntaggedContent(#[serde(with = "either::serde_untagged")] MessageContent);
-//     let mut new_messages = Vec::new();
-//     for message in messages {
-//         let mut new_message = IndexMap::new();
-//         for (k, v) in message {
-//             new_message.insert(k, UntaggedContent(v));
-//         }
-//         new_messages.push(new_message);
-//     }
-
-//     let template = match &template.0 {
-//         Either::Left(x) => x.clone(),
-//         Either::Right(map) => {
-//             let mut template = "".to_string();
-//             for t in map {
-//                 if t.contains_key("tool_use") && !tools.is_empty() {
-//                     template = t["tool_use"].clone();
-//                     break;
-//                 } else if t.contains_key("default") {
-//                     template = t["default"].clone();
-//                     break;
-//                 }
-//             }
-//             if template.is_empty() {
-//                 anyhow::bail!("Chat template does not contain a `tool_use` or `default` key. Please ensure it contains at least a `default` key, although `tool_use` should be specified for using tools.");
-//             }
-//             template
-//         }
-//     };
-
-//     env.add_template("chat_template", &template)?;
-//     env.add_function("raise_exception", raise_exception);
-//     env.add_filter("tojson", tojson);
-//     let tmpl = env.get_template("chat_template").unwrap();
-
-//     let date = chrono::Utc::now();
-//     let date_string = date.format("%d, %B, %Y").to_string();
-
-//     if tools.is_empty() {
-//         Ok(tmpl.render(context! {
-//             messages => new_messages,
-//             add_generation_prompt => add_generation_prompt,
-//             bos_token => bos_tok,
-//             eos_token => eos_tok,
-//             unk_token => unk_tok,
-//             date_string => date_string,
-//         })?)
-//     } else {
-//         Ok(tmpl.render(context! {
-//             messages => new_messages,
-//             add_generation_prompt => add_generation_prompt,
-//             bos_token => bos_tok,
-//             eos_token => eos_tok,
-//             unk_token => unk_tok,
-//             tools => tools,
-//             date_string => date_string,
-//         })?)
-//     }
-// }
