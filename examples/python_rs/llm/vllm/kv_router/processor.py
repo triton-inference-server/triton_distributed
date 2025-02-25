cd515f1a2be6d00ba9d4bbf96ef4fb579ea83f9e
nnshah1 <neelays@nvidia.com>
2025-02-25 06:56:27 -0800

    updating to export client

--- examples/python_rs/llm/vllm/kv_router/processor.py
+++ examples/python_rs/llm/vllm/kv_router/processor.py
@@ -31,8 +31,8 @@ from vllm.logger import logger as vllm_logger
 from vllm.outputs import RequestOutput
 from vllm.transformers_utils.tokenizer import AnyTokenizer
 
-from triton_distributed._core import Client
 from triton_distributed.runtime import (
+    Client,
     DistributedRuntime,
     triton_endpoint,
     triton_worker,
