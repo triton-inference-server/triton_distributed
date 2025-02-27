328642dce1911eef2a8ea6029db57f0fcec9aee9
Harrison King Saturley-Hall <hsaturleyhal@nvidia.com>
2025-02-27 09:39:43 -0500

    ci: docker alias to buildx

--- container/build.sh
+++ container/build.sh
@@ -320,7 +320,7 @@ if [ -z "$RUN_PREFIX" ]; then
     set -x
 fi
 
-$RUN_PREFIX docker buildx build -f $DOCKERFILE $TARGET_STR $PLATFORM $BUILD_ARGS $CACHE_FROM $CACHE_TO $TAG $LATEST_TAG --output type=docker $BUILD_CONTEXT $NO_CACHE
+$RUN_PREFIX docker build -f $DOCKERFILE $TARGET_STR $PLATFORM $BUILD_ARGS $CACHE_FROM $CACHE_TO $TAG $LATEST_TAG --output type=docker $BUILD_CONTEXT $NO_CACHE
 
 { set +x; } 2>/dev/null
 
